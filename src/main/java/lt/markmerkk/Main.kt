8ce8e593ef5db9036ad5b957f7a02d32ebd44bd0
Marius Merkevicius <mariusmerkevicius@Mariuss-MacBook-Pro-2.local>
2016-11-05 23:40:59 +0200

    Init

---
+++ src/main/java/lt/markmerkk/Main.kt
@@ -0,0 +1,12 @@
+package lt.markmerkk
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello world!")
+        }
+    }
+}
+
+
